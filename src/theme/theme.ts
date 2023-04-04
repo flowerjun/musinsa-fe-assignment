f8ae540169e3d28186efe200d9dee51a4f5b3ac9
golee.hyper-cloud <FLzhdms147^^>
2023-04-04 22:57:07 +0900

    FEAT: font 및 color theme 파일로 분리

---
+++ src/theme/theme.ts
@@ -0,0 +1,20 @@
+export const color = {
+  white: '#FFF',
+  black: '#000',
+  green: '#18A286',
+  darkGray: '#777',
+  dimWhite80: 'rgba(255, 255, 255, 0.8)',
+  gray1: '#F9F9F9',
+  gray4: '#EEE',
+  gray5: '#CCC',
+  oldGray3: '#F1F1F1',
+  oldGray6: '#AAA',
+  fRed1: '#FF0000',
+  accent: '#0078FF',
+};
+
+export const font = {
+  defaultSize: 14,
+  subSize: 11,
+  largeSize: 22,
+};
