d90858af6480de078514a4e0c3c8765dc5530451
golee.hyper-cloud <FLzhdms147^^>
2023-04-04 22:59:05 +0900

    FEAT: 로고 타이틀 추가

---
+++ src/components/header/HeaderTitle.tsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+import { color } from 'src/theme/theme';
+import { iconImagePath, SVGIcon } from 'src/assets/assets-path';
+
+// ----------------------------------------------------------------------
+
+const HeaderTitle = styled.header`
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  top: 0;
+  left: 50%;
+  transform: translate(-50%, 0);
+
+  width: 375px;
+  height: 50px;
+  background-color: ${color.white};
+  z-index: 100;
+`;
+
+export default function Header() {
+  return (
+    <HeaderTitle>
+      <SVGIcon
+        icon={iconImagePath.logo}
+        boxSize={{ width: 94.89, height: 16 }}
+        viewBox={{ minX: 0, minY: 0, width: 94.89, height: 16 }}
+        fill={color.black}
+      />
+    </HeaderTitle>
+  );
+}
