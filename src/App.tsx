9517c32a0a12d23e2eaa15a890f68008a9833193
golee.hyper-cloud <FLzhdms147^^>
2023-04-07 02:09:41 +0900

    FEAT: App 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,108 @@
-import React, { useEffect, useState } from "react";
-import styled from "styled-components";
+import { useEffect, useRef, useState } from 'react';
+import styled, { keyframes } from 'styled-components';
 
-import Goods from "./@types/Goods";
-import Header from "./components/Header";
-import ProductImage from "./components/ProductImage";
-import { BASE_URL, MAX_PAGE } from "./constants/constants";
-import useInfiniteScroll from "./hooks/useInfinteScroll";
+import { IFilter } from './@types/Filter';
+import { IGoods } from './@types/Goods';
+import { fetchGoods } from './api/fetchGoods';
+import { iconImagePath, SVGIcon } from './assets/assets-path';
+import GoodsImage from './components/goods/GoodsImage';
+import GoodsInfo from './components/goods/GoodsInfo';
+import HeaderFilters from './components/header/HeaderFilters';
+import HeaderTitle from './components/header/HeaderTitle';
+import useInfiniteScroll from './hooks/useInfinteScroll';
+import GlobalStyle from './theme/GlobalStyle';
+import { color } from './theme/theme';
+import uuidv4 from './util/uuidv4';
+
+// ----------------------------------------------------------------------
 
 const AppWrapper = styled.div`
-  display: flex;
   flex-direction: column;
-  height: 100%;
-`;
-
-const Filter = styled.div`
-  position: fixed;
-  top: 50px;
-  left: 0;
-  width: 100%;
-  background-color: #fff;
-  z-index: 100;
-  padding: 10px 7px;
+  width: 375px;
+  height: auto;
+  margin: 0 auto;
 `;
 
 const GoodsListWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
-  justify-content: center;
-  margin-top: 105px;
+  width: 100%;
+  margin-top: 115px;
 `;
 
-interface IFilter {
-  exclusive: boolean;
-  sale: boolean;
-  soldOut: boolean;
-  searchKeyword: string;
-}
+const rotate = keyframes`
+  0%{
+    transform: rotate(0deg);
+  }
+  100%{
+    transform: rotate(360deg);
+  }
+`;
 
+const LoadingWrapper = styled.div`
+  display: flex;
+  position: relative;
+`;
+
+const Loading = styled.div`
+  position: absolute;
+  width: 14px;
+  height: 14px;
+  bottom: 0;
+  left: 50%;
+  transform: translate(-50%, 0);
+
+  animation: ${rotate} 1s linear infinite;
+`;
+
+const NotFoundWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 375px;
+  height: calc(100vh - 50px);
+  justify-content: center;
+  align-items: center;
+
+  color: ${color.oldGray6};
+`;
 
 export default function App() {
-  const [goodsList, setGoodsList] = useState<Goods[]>([]);
-  const [filteredGoodsList, setFilteredGoodsList] = useState<Goods[]>([]);
   const [page, setPage] = useState<number>(0);
+  const [goodsList, setGoodsList] = useState<IGoods[]>([]);
+  const [filteredGoodsList, setFilteredGoodsList] = useState<IGoods[]>([]);
+  const [isLastPage, setIsLastPage] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [filter, setFilter] = useState<IFilter>({
     exclusive: false,
     sale: false,
     soldOut: false,
-    searchKeyword: "",
+    search: false,
+    searchKeyword: '',
   });
 
-  const fetchGoods = async () => {
-    try {
-      if(page < MAX_PAGE){
-        const response = await fetch(`${BASE_URL}${page}.json`);
-        const goodsList = await response.json().then(res => res.data.list);
-
-        setPage(page + 1);
-        setGoodsList((prevGoodsList) => [...prevGoodsList, ...goodsList]);
-        setFilteredGoodsList((prevFilteredGoodsList) => [...prevFilteredGoodsList, ...goodsList]);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  
-  const [loaderRef, isIntersecting] = useInfiniteScroll(fetchGoods);
+  const intersectRef = useRef<HTMLDivElement>(null);
+  const { isIntersecting } = useInfiniteScroll(intersectRef, {
+    rootMargin: '0px',
+    threshold: 0.6,
+  });
 
   useEffect(() => {
-    const filteredGoods = goodsList.filter((goods) => {
-      if (filter.searchKeyword && !goods.goodsName.includes(filter.searchKeyword)) {
+    fetchMoreGoods();
+  }, []);
+
+  useEffect(() => {
+    if (!isLastPage && !isLoading && isIntersecting) fetchMoreGoods();
+  }, [isLastPage, isLoading, isIntersecting]);
+
+  useEffect(() => {
+    const filteredGoods = goodsList.filter((goods: IGoods) => {
+      if (
+        filter.searchKeyword &&
+        !(
+          goods.goodsName.includes(filter.searchKeyword) ||
+          goods.brandName.includes(filter.searchKeyword)
+        )
+      ) {
         return false;
       }
 
@@ -90,6 +124,23 @@ export default function App() {
     setFilteredGoodsList(filteredGoods);
   }, [goodsList, filter]);
 
+  const fetchMoreGoods = async () => {
+    if (isLoading) return;
+
+    setIsLoading(true);
+
+    const loadedGoods = await fetchGoods(page);
+
+    if (loadedGoods.length === 0) {
+      setIsLastPage(true);
+    } else {
+      setGoodsList((prev) => [...prev, ...loadedGoods]);
+      setPage((prev) => prev + 1);
+    }
+
+    setIsLoading(false);
+  };
+
   const handleFilterChange = (name: keyof IFilter) => {
     setFilter((prevState) => ({
       ...prevState,
@@ -97,74 +148,64 @@ export default function App() {
     }));
   };
 
-  const handleSearchInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = event.target;
-
-    setFilter((prevState) => ({
-      ...prevState,
-      searchKeyword: value,
-    }));
-  };
-
   return (
     <AppWrapper>
-      <Header/>
-      <Filter>
-        <div>
-          <label htmlFor="search">검색</label>
-          <input
-            type="text"
-            id="search"
-            value={filter.searchKeyword}
-            onChange={handleSearchInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="sale">세일상품</label>
-          <input
-            type="checkbox"
-            id="sale"
-            checked={filter.sale}
-            onChange={() => handleFilterChange("sale")}
-          />
-        </div>
-        <div>
-          <label htmlFor="exclusive">단독상품</label>
-          <input
-            type="checkbox"
-            id="exclusive"
-            checked={filter.exclusive}
-            onChange={() => handleFilterChange("exclusive")}
-          />
-        </div>
-        <div>
-          <label htmlFor="soldOut">품절포함</label>
-          <input
-            type="checkbox"
-            id="soldOut"
-            checked={filter.soldOut}
-            onChange={() => handleFilterChange("soldOut")}
-          />
-        </div>
-      </Filter>
+      <GlobalStyle />
+
+      <HeaderTitle />
+      <HeaderFilters
+        goods={filteredGoodsList}
+        filter={filter}
+        setFilter={setFilter}
+        handleFilterChange={handleFilterChange}
+      />
 
       <GoodsListWrapper>
-        {filteredGoodsList.map((goods: Goods, i: number) => (
-          <div key={goods.goodsNo + i}>
-            <ProductImage imageUrl={goods.imageUrl} alt={goods.goodsName} />
-            <p>{goods.goodsName}</p>
-            <p>{goods.normalPrice}</p>
-            {goods.isSale && <p style={{ backgroundColor: 'blue'}}>{goods.price}</p>}
-            {goods.isExclusive && <p style={{ backgroundColor: 'yellow'}}>단독</p>}
-            {goods.isSoldOut && <p style={{ backgroundColor: 'red'}}>Sold Out</p>}
-          </div>
+        {filteredGoodsList.length > 0 && !isLoading ? (
+          filteredGoodsList.map((goods: IGoods) => (
+            <div key={uuidv4()}>
+              <GoodsImage
+                imageUrl={goods.imageUrl}
+                alt={goods.goodsName}
+                linkUrl={goods.linkUrl}
+                isSoldOut={goods.isSoldOut}
+              />
+
+              <GoodsInfo
+                goodsName={goods.goodsName}
+                price={goods.price}
+                brandName={goods.brandName}
+                linkUrl={goods.linkUrl}
+                brandLinkUrl={goods.brandLinkUrl}
+                normalPrice={goods.normalPrice}
+                isSale={goods.isSale}
+                saleRate={goods.saleRate}
+                isExclusive={goods.isExclusive}
+              />
+            </div>
           ))
-        }
+        ) : (
+          <NotFoundWrapper>
+            <SVGIcon
+              icon={iconImagePath.notFoundIcon}
+              size={90}
+              style={{ marginBottom: '4px' }}
+              boxSize={{ width: 90, height: 90 }}
+              viewBox={{ width: 90, height: 90, minX: 0, minY: 0 }}
+            />
+
+            <span>검색 결과 없음</span>
+          </NotFoundWrapper>
+        )}
       </GoodsListWrapper>
 
-      <div ref={loaderRef}>
-        {isIntersecting && page < MAX_PAGE && <p>Loading...</p>}
-      </div>
+      {!isLastPage && !isLoading && (
+        <LoadingWrapper ref={intersectRef}>
+          <Loading>
+            <SVGIcon icon={iconImagePath.loadingIcon} size={16} />
+          </Loading>
+        </LoadingWrapper>
+      )}
     </AppWrapper>
   );
-}
+};
